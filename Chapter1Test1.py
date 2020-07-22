00cf60601c5cc8d2137c27f6ed4feab01810a4d5
NoTTa1 <68268707+inwnote154@users.noreply.github.com>
2020-07-22 18:38:09 +0700

    Add files via upload

---
+++ Chapter1Test1.py
@@ -0,0 +1,11 @@
+#6306021620018 นายนวพรรษ นกงาม sec A
+#การบ้านข้อที่ 1 & 2
+name = input("Enter name : ")
+surname = input("Enter Surname : ")
+age = input("Enter age : ")
+
+
+print("My name is "+name)
+print("My surname is "+surname )
+print("Now I\'m "+str(age) )
+print("I\'m finish undergraduate in "+str(int(age)+4)+" years old")
---
+++ Chapter2Test2.py
@@ -0,0 +1,7 @@
+A=10
+B=20
+print("Before Swap A =",A,", B =",B)
+B=A+B
+A=B-A
+B=B-A
+print("After Swap A =",A,", B =",B)
