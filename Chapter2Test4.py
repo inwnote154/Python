41e1596e7dc2dc986d0bab3143f7080ce7b59319
NoTTa1 <68268707+inwnote154@users.noreply.github.com>
2020-07-29 17:31:18 +0700

    Update Chapter2Test4.py

--- Chapter2Test4.py
+++ Chapter2Test4.py
@@ -1,9 +1,9 @@
 Sum  = 0
 
 Sum += int(input("Enter number 1 : "))
-Sum += int(input("Enter number 1 : "))
-Sum += int(input("Enter number 1 : "))
-Sum += int(input("Enter number 1 : "))
+Sum += int(input("Enter number 2 : "))
+Sum += int(input("Enter number 3 : "))
+Sum += int(input("Enter number 4 : "))
 
 Average = Sum / 4
 print()
