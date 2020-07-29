76a111ac9b8375bd3500874acd7a7773fc4af48d
NoTTa1 <68268707+inwnote154@users.noreply.github.com>
2020-07-29 17:24:20 +0700

    Add files via upload

---
+++ Chapter2Test4.py
@@ -0,0 +1,11 @@
+Sum  = 0
+
+Sum += int(input("Enter number 1 : "))
+Sum += int(input("Enter number 1 : "))
+Sum += int(input("Enter number 1 : "))
+Sum += int(input("Enter number 1 : "))
+
+Average = Sum / 4
+print()
+print("Summation =",Sum)
+print("Average =",Average)
