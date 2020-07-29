e30e5a1bb3b540d8d85d5afdd409935f5c18faed
NoTTa1 <68268707+inwnote154@users.noreply.github.com>
2020-07-29 17:25:02 +0700

    Delete Chapter1Test3.py

--- Chapter1Test3.py
+++
@@ -1,8 +0,0 @@
-int1 = input("Enter float number 1 : ")
-int2 = input("Enter float number 2 : ")
-int3 = input("Enter float number 3 : ")
-
-print("Value number 1 : " +str(float(int1)))
-print("Value number 2 : " +str(float(int2)))
-print("Value number 3 : " +str(float(int3)))
-print("Total all : "+str(float(int1)+float(int2)+float(int3)))
